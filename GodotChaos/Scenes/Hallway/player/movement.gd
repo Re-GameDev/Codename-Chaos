cfd8a625f121034b70147a19e78b981bb995cc98
strawstack <rhshadowgreen@gmail.com>
2022-05-06 14:52:55 -0700

    Create hallway scene. Add hallway scene button to main menu

---
+++ GodotChaos/Scenes/Hallway/player/movement.gd
@@ -0,0 +1,43 @@
+extends KinematicBody
+
+onready var camera = $Pivot/Camera
+
+var gravity = -30
+var max_speed = 8
+var mouse_sensitivity = 0.002  # radians/pixel
+
+var velocity = Vector3()
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func get_input():
+	var input_dir = Vector3()
+	# desired move in camera direction
+	if Input.is_action_pressed("forward"):
+		input_dir += -global_transform.basis.z
+	if Input.is_action_pressed("right"):
+		input_dir += global_transform.basis.x
+	if Input.is_action_pressed("back"):
+		input_dir += global_transform.basis.z
+	if Input.is_action_pressed("left"):
+		input_dir += -global_transform.basis.x
+	if Input.is_action_just_pressed("exit"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	input_dir = input_dir.normalized()
+	return input_dir
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	var desired_velocity = get_input() * max_speed
+
+	velocity.x = desired_velocity.x
+	velocity.z = desired_velocity.z
+	velocity = move_and_slide(velocity, Vector3.UP, true)
