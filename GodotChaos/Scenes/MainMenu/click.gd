48e81caeba954e42ef49bbf9be465b778daf8759
strawstack <rhshadowgreen@gmail.com>
2022-05-18 16:37:36 -0700

    Change how scenes are loaded when btns on main menu are pushed

--- GodotChaos/Scenes/MainMenu/click.gd
+++ GodotChaos/Scenes/MainMenu/click.gd
@@ -1,5 +1,7 @@
 extends Button
 
+export (String) var pathToScene
+
 func _on_Button_pressed():
 	#warning-ignore:RETURN_VALUE_DISCARDED
-	get_tree().change_scene("res://Scenes/Grocery/grocery.tscn")
+	get_tree().change_scene(pathToScene)
